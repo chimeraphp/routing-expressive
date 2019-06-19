93029262f8aab9596c002798f4c26303c04cdc0b
Luís Cobucci <lcobucci@gmail.com>
2019-06-19 17:25:30 +0200

    Provide adapter for Zend Expressive

---
+++ src/Application.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace Chimera\Routing\Expressive;
+
+use Chimera\Routing\Application as ApplicationInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Expressive\Application as Expressive;
+
+final class Application implements ApplicationInterface
+{
+    /**
+     * @var Expressive
+     */
+    private $application;
+
+    public function __construct(Expressive $application)
+    {
+        $this->application = $application;
+    }
+
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return $this->application->handle($request);
+    }
+
+    public function run(): void
+    {
+        $this->application->run();
+    }
+}
